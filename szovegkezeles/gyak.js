d0efdd768600bdfb80e92dbb25f38a0b890d8484
borcsokdavid <55079354+borcsokdavid@users.noreply.github.com>
2019-10-21 13:17:03 +0200

    Add files via upload

---
+++ szovegkezeles/gyak.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function valtoztat(elem) {
+    let tartalom =  elem.innerHTML;
+    let tomb = tartalom.split(' ');
+    elem.innerHTML = "Ennek a tömbnek a hossza:" + tomb.length ;
+    let szoveg = "<ul>";
+    for (let i = 0; i < tomb.length; i++) {
+        szoveg += "<li>";
+        for (let j = 0; j < tomb[i].length; j++) {
+            
+            if (j == 0) {
+                szoveg += `${tomb[i].charAt(0).toUpperCase()} `;
+            } else {
+                szoveg += `${tomb[i].charAt(j)} `;
+            }
+            
+        }
+        
+        szoveg += "<li>";
+    }
+    szoveg += "</ul>";
+    elem.innerHTML = szoveg
+    
+    
+}
